0aaa720f2470a60f8019385493a5754e8e976459
pen. ciun. jian <shuiruge@whu.edu.cn>
2018-03-18 17:36:51 +0800

    Update finetuner.py

--- lgd/finetuner.py
+++ lgd/finetuner.py
@@ -5,6 +5,8 @@
 
 import abc
 import tensorflow as tf
+from copy import deepcopy
+import inspect
 
 
 class BaseOptimizee(abc.ABC):
@@ -30,6 +32,35 @@ class BaseOptimizee(abc.ABC):
     with tf.name_scope(self.name):
       with tf.name_scope(name):
         pass
+
+
+def intend_back(n_intends, source):
+  new_source = ''
+  s = -1
+  for c in source[2:]:
+    if c == '\n':
+      new_source += c
+      s = 0
+    elif s >= 0 and s < n_intends:
+      s += 1
+      continue
+    else:
+      new_source += c
+      s = -1
+  return new_source
+
+
+def drop_convert_to_tensor(optimizer):
+  def id(x, *args, **kwargs):
+    return x
+  source = inspect.getsource(optimizer._prepare)
+  new_source = source.replace('self', 'optimizer')
+  new_source = new_source.replace('ops.convert_to_tensor', 'id')
+  new_source = intend_back(2, new_source)
+  print(new_source)
+  loc = {}
+  exec(new_source, {}, loc)  # which generates function `_prepare`.
+  return loc['_prepare']
 
 
 class FineTuner(object):
@@ -61,7 +92,9 @@ class FineTuner(object):
       ]
       self.loss, self.gradients = optimizee\
           .make_loss_and_gradients(*self.variables)
-      self.optimizer = optimizer(*self.parameters)
+      _optimizer = deepcopy(optimizer)
+      _optimizer._prepare = drop_convert_to_tensor(_optimizer)
+      self.optimizer = _optimizer(*self.parameters)
       # XXX: notice that args in `tf.train.Optimizer.__init__()` will be
       # converted to tensor by `tf.convert_to_tensor()` in its `_prepare()`.
       # This may cause error, to be revealed.
@@ -84,11 +117,11 @@ class FineTuner(object):
 
       with tf.name_scope(name):
 
-        for i, var in enumerate(self.variables):
-          reset_ops.append(var.assign(variables[i]))
+        for var, var_val in list(zip(self.variables, variables)):
+          reset_ops.append(var.assign(var_val))
 
-        for i, param in enumerate(self.parameters):
-          reset_ops.append(param.assign(parameters[i]))
+        for param, param_val in list(zip(self.parameters, parameters)):
+          reset_ops.append(param.assign(param_val))
 
     return tf.group(reset_ops)
 
@@ -102,6 +135,7 @@ class FineTuner(object):
         # `tf.train.Optimizer.apply_gradients()` only updates the
         # `tf.Variable`s within its argument `grad_and_vars`
         update_op = self.optimizer.apply_gradients(self.gradients)
+        print('hahaha', self.optimizer._learning_rate_tensor)
 
         def cond(trial, meta_loss):
           return tf.less(trial, n_trials)
@@ -110,7 +144,7 @@ class FineTuner(object):
           # Ensure that `update_op` is called before adding `loss`
           # to `meta_loss`
           with tf.control_dependencies([update_op]):
-            return trial + 1, meta_loss + loss
+            return trial + 1, meta_loss + self.loss
 
         _, meta_loss = tf.while_loop(cond, body, [0, 0.0])
 
@@ -134,22 +168,28 @@ class FineTuner(object):
 
 if __name__ == '__main__':
 
+  tf.reset_default_graph()
 
   class TestOptimizee(BaseOptimizee):
 
     def make_loss_and_gradients(self, x):
       loss = tf.square(x)
-      g = tf.gradients(loss, [x])
+      g, *rests = tf.gradients(loss, [x])
       gradients = [(g, x)]
       return loss, gradients
 
   def test():
+
     optimizee = TestOptimizee()
     optimizer = tf.train.GradientDescentOptimizer
+
     x = tf.Variable(initial_value=1000.0, dtype='float32')
+    x_t = tf.placeholder(shape=[], dtype='float32')
     lr = tf.Variable(initial_value=0.01, dtype='float32')
+    lr_t = tf.placeholder(shape=[], dtype='float32')
+
     finetuner = FineTuner(optimizee=optimizee, optimizer=optimizer,
-                          variables=[x], parameters=[lr])
+                          variables=[x_t], parameters=[lr_t])
 
     tune_op = finetuner.tune()
 
@@ -157,15 +197,10 @@ if __name__ == '__main__':
 
       for i in range(10):
 
-        _, lr_val = sess.run([tune_op, lr])
+        _, lr_val = sess.run([tune_op] + finetuner.parameters,
+                             feed_dict={x_t: 100, lr_t: 0.01})
 
         print(lr_val)
 
 
   test()
-
-
-
-
-
-      
